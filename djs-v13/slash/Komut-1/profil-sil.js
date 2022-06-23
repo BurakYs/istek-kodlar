3399a713c2a7579e59e3a80d4d1b982000f18c6b
Burak <59135244+BurakYs@users.noreply.github.com>
2022-06-23 16:50:10 +0300

    Update profil-sil.js

--- djs-v13/slash/Komut-1/profil-sil.js
+++ djs-v13/slash/Komut-1/profil-sil.js
@@ -1,12 +1,8 @@
-const config = require("../../config.js")
-const { MessageEmbed, MessageActionRow, MessageButton, Message } = require("discord.js")
 const { JsonDatabase } = require("wio.db");
-
 const db = new JsonDatabase({
     databasePath: "./database.json"
 });
 module.exports = {
-
     name: 'profil-sil',
     description: 'Kullanıcının profilini silersiniz',
     category: 'Moderasyon',
@@ -17,52 +13,38 @@ module.exports = {
             description: 'Profilini sileceğiniz kullanıcı',
             required: true,
         },
-     ],
-
+    ],
     requiredPermissions: [],
     requiredPermissionsMe: [],
-
     disabled: false,
     developerOnly: false,
-
     run: async (client, interaction) => {
-    
-let user = interaction.options.getUser("kullanici")
-if(interaction.member.roles.cache.has("Profil Kontrol Yetkili Rol ID")) return;
-
-let member = interaction.guild.members.cache.get(user.id)
-let meslek = db.get(`profil_${user.id}.meslek`)
-let din = db.get(`profil_${user.id}.din`)
-let cins = db.get(`profil_${user.id}.cins`)
-let yonelim = db.get(`profil_${user.id}.yonelim`)
-
-
-        if (meslek === "Polis Memuru") { member.roles.remove("Polis Memuru Rol ID") }
-        if (meslek === "Avukat") { member.roles.remove("Avukat Rol ID")) }
-        if (meslek === "Savcı") { member.roles.remove("Savcı Rol ID") }
-        if (meslek === "Yargıç") { member.roles.remove("Yargıç Rol ID") }
-        if (meslek === "Doktor") { member.roles.remove("Doktor Rol ID") }
-        if (meslek === "Psikolog") { member.roles.remove("Psikolog Rol ID") }
-        if (meslek === "Hemşire") { member.roles.remove("Hemşire Rol ID") }
-        if (meslek === "Mafya") { member.roles.remove("Mafya Rol ID") }
-        if (meslek === "Çete") { member.roles.remove("Çete Rol ID") }
-        if (meslek === "Eskort") { member.roles.remove("Eskort Rol ID") }
-        if (yonelim === "Hetero") { member.roles.remove("Hetero Rol ID") }
-        if (yonelim === "Homo") { member.roles.remove("Homo Rol ID") }
-        if (yonelim === "Bi") { member.roles.remove("Bi Rol ID") }
-        if (yonelim === "Aseksüel") { member.roles.remove("Aseksüel Rol ID") }
-        if (cins === "Erkek") { member.roles.remove("Erkek Rol ID") }
-        if (cins === "Kadın") { member.roles.remove("Kadın Rol ID") }
-        if (din === "Hristiyan") { member.roles.remove("Hristiyan Rol ID") }
-        if (din === "Müslüman") { member.roles.remove("Müslüman Rol ID") }
-        if (din === "Yahudi") { member.roles.remove("Yahudi Rol ID") }
-        if (din === "Ateist") { member.roles.remove("Ateist Rol ID") }
-        interaction.member.roles.remove("Kayıtlı Rol ID")
-        interaction.member.roles.add("Kayıtsız Rol ID")
-   await interaction.reply({embeds: [client.successEmbed.setDescription(`${user} kullanıcısının profili silindi`)]})
-   db.findAndDelete((element,db) => {
-    return element.ID.includes(`profil_${user.id}`);
-});
+        let user = interaction.options.getUser("kullanici")
+        let member = interaction.guild.members.cache.get(user.id)
+        member.roles.remove("Yargıç Rol ID")
+        member.roles.remove("Doktor Rol ID")
+        member.roles.remove("Psikolog Rol ID")
+        member.roles.remove("Hemşire Rol ID")
+        member.roles.remove("Mafya Rol ID")
+        member.roles.remove("Çete Rol ID")
+        member.roles.remove("Eskort Rol ID")
+        member.roles.remove("Hetero Rol ID")
+        member.roles.remove("Homo Rol ID")
+        member.roles.remove("Bi Rol ID")
+        member.roles.remove("Aseksüel Rol ID")
+        member.roles.remove("Erkek Rol ID")
+        member.roles.remove("Kadın Rol ID")
+        member.roles.remove("Hristiyan Rol ID")
+        member.roles.remove("Müslüman Rol ID")
+        member.roles.remove("Ateist Rol ID")
+        member.roles.remove("Yahudi Rol ID")
+        member.roles.remove("Ateist Rol ID")
+        member.roles.remove("Kayıtlı Rol ID")
+        member.roles.add("Kayıtsız Rol ID")
+        await interaction.reply({ embeds: [client.successEmbed.setDescription(`${user} kullanıcısının profili silindi`)] })
+        db.findAndDelete((element, db) => {
+            return element.ID.includes(`profil_${user.id}`);
+        });
     },
 };
-
+	
